4fadc2361e86527b6f25bc4f86f64119ef7d3fcc
Sara Diaz <sara.diaz052@gmail.com>
2020-01-23 20:42:06 +0100

    added new main class

---
+++ src/LP.java
@@ -0,0 +1,18 @@
+import org.apache.giraph.graph.BasicComputation;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+
+import org.apache.giraph.conf.LongConfOption;
+import org.apache.giraph.edge.Edge;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+public class LP extends BasicComputation<LongWritable, DoubleWritable, FloatWritable, DoubleWritable>{
+
+
+        }
