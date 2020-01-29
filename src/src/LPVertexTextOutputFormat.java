2e22eb7cb232c047cc28955c67c2cf9d7b0e3e96
Sara Diaz <sara.diaz052@gmail.com>
2020-01-29 23:38:07 +0100

    added new structure

---
+++ src/src/LPVertexTextOutputFormat.java
@@ -0,0 +1,31 @@
+package src;
+
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.giraph.io.formats.TextVertexOutputFormat;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+import java.io.IOException;
+
+public class LPVertexTextOutputFormat  extends TextVertexOutputFormat <LongWritable, VertexValue, FloatWritable>
+{
+    @Override
+    public TextVertexWriter createVertexWriter(TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException
+    {
+        return null;
+    }
+
+    private class LPTextVertexLineWriter extends TextVertexWriterToEachLine
+    {
+        @Override
+        protected Text convertVertexToLine(Vertex<LongWritable, VertexValue, FloatWritable> vertex) throws IOException
+        {
+            StringBuilder sb = new StringBuilder(vertex.getId().toString());
+            sb.append(" ");
+            sb.append(vertex.getValue().getActualCommunity().get());
+            return new Text(sb.toString());
+        }
+    }
+}
